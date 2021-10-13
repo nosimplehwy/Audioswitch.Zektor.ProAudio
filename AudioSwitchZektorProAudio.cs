5be01e810934b8276d279110f94cc9511b1e2e17
Jen Edelstein <jedelstein@ati-america.com>
2021-10-13 14:51:45 -0600

    initial add

---
+++ AudioSwitchZektorProAudio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crestron.RAD.Common.BasicDriver;
+using Crestron.RAD.Common.Enums;
+using Crestron.RAD.Common.Events;
+using Crestron.RAD.Common.Interfaces;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.Reflection;
+using Crestron.RAD.DeviceTypes.AudioVideoSwitcher;
+using Crestron.RAD.Common.Transports;
+using Independentsoft.Exchange;
+
+namespace AudioSwitchZektorProAudio
+{
+    public class AudioSwitchZektorProAudio : AAudioVideoSwitcher, ITcp
+    {
+        public AudioSwitchZektorProAudio()
+        {
+            
+        }
+
+        #region ITcp Members
+
+        public void Initialize(IPAddress ipAddress, int port)
+        {
+            var tcpTransport = new TcpTransport()
+            {
+                EnableAutoReconnect = EnableAutoReconnect,
+                EnableLogging = InternalEnableLogging,
+                CustomLogger = InternalCustomLogger,
+                EnableRxDebug = InternalEnableRxDebug,
+                EnableTxDebug = InternalEnableTxDebug
+            };
+
+            tcpTransport.Initialize(ipAddress,port);
+            ConnectionTransport = tcpTransport;
+
+            AudioVideoSwitcherProtocol = new ZektorProAudioProtocol(ConnectionTransport, Id)
+            {
+                EnableLogging = InternalEnableLogging,
+                CustomLogger = InternalCustomLogger
+            };
+            AudioVideoSwitcherProtocol.RxOut += SendRxOut;
+            AudioVideoSwitcherProtocol.Initialize(AudioVideoSwitcherData);
+        }
+
+        #endregion
+
+    }
+}
---
+++ ZektorProAudioProtocol.cs
@@ -0,0 +1,14 @@
+﻿using Crestron.RAD.Common.Transports;
+using Crestron.RAD.DeviceTypes.AudioVideoSwitcher;
+
+namespace AudioSwitchZektorProAudio
+{
+    public class ZektorProAudioProtocol : AAudioVideoSwitcherProtocol
+    {
+        public ZektorProAudioProtocol(ISerialTransport transport, byte id)
+        : base(transport, id)
+        {
+            
+        }
+    }
+}
