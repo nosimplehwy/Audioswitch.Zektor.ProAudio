3b080cdaa9a263d8a139023b47b5477a53ea1e01
jedelstein_ati <jedelstein@ati-america.com>
2021-10-21 17:00:34 -0600

    added libraries from crestron samples

---
+++ DriverLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Crestron.RAD.Common.Logging;
+using Crestron.SimplSharp;
+
+namespace AudioSwitchZektorProAudio
+{
+    public class DriverLog
+    {
+
+        public static void Log(bool logEnabled, Action<string> logMethod, LoggingLevel logLevel, string methodName, string message)
+        {
+            var logMessage = $"{logLevel}: {methodName} - {message}";
+
+            if (logEnabled)
+            {
+                switch (logLevel)
+                {
+                    case LoggingLevel.Error:
+                        logMethod(logMessage);
+                        ErrorLog.Error(logMessage);
+                        break;
+
+                    case LoggingLevel.Warning:
+                        logMethod(logMessage);
+                        ErrorLog.Warn(logMessage);
+                        break;
+
+                    case LoggingLevel.Debug:
+                        logMethod(logMessage);
+                        ErrorLog.Notice(logMessage);
+                        break;
+                }
+            }
+
+        }
+    }
+}
